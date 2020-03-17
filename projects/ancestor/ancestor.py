d732caec0aef6b4ad2c2b36fcd92a335ef34d613
scottwmwork <scottwmwork@users.noreply.github.com>
2020-03-17 14:41:32 -0700

    Finished earliest_ancestor

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,3 +1,31 @@
 
 def earliest_ancestor(ancestors, starting_node):
-    pass
+    visited = set()
+    found_earliest = False
+    depth = 0
+    while found_earliest is False:
+        count = 0
+        for pair in ancestors:
+            if pair not in visited:
+                if starting_node == pair[1]:
+                    visited.add(pair)
+                    starting_node = pair[0]
+                    count += 1
+                    depth += 1
+                    break
+
+        if count == 0:
+            if depth == 0:
+                return -1
+            else:
+                found_earliest = True
+
+    return starting_node
+                
+            
+
+            
+            
+
+
+        
